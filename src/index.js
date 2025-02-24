e5df15b04b5d73a7e3791da12c4edbd8e4a260ad
cyberpeony <fernandadiaz@Fernandas-MacBook-Pro-4.local>
2025-02-24 08:24:35 -0600

    minor interface changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function calculate() {
+function sum() {
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
 
@@ -8,9 +8,19 @@ function calculate() {
     document.getElementById("result").innerText = "Result: " + result;
 }
 
+function substract() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    let result;
+    result = Number(num1) - Number(num2);
+
+    document.getElementById("result").innerText = "Result: " - result;
+}
+
 function resetFields(){
     document.getElementById("num1").value ="";
     document.getElementById("num2").value ="";
-    document.getElementById("operator").value ="+";
+    document.getElementById("operator").value ="";
     document.getElementById("result").innerText ="Result: ";
 }
