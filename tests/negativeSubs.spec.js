aaef8b1c8aea2efbd31348b31b7cb33066c2ecc9
cyberpeony <fernandadiaz@Fernandas-MacBook-Pro-4.local>
2025-02-24 15:48:11 -0600

    new tests for the substraction

--- tests/negativeSubs.spec.js
+++ tests/negativeSubs.spec.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 const fs = require('fs');
 
-describe('-1+-1', function () {
+describe('-1 - (-2) Test', function () {
     this.timeout(30000);
     let driver;
     let vars;
@@ -32,17 +32,17 @@ describe('-1+-1', function () {
         }
     });
 
-    it('-1+-1', async function () {
+    it('-1-(-2)', async function () {
         await driver.get("http://127.0.0.1:8000/index.html")
         await driver.manage().window().setRect({ width: 720, height: 900 });
 
         await driver.findElement(By.id("num1")).click();
-        await driver.findElement(By.id("num1")).sendKeys("-3");
+        await driver.findElement(By.id("num1")).sendKeys("-2");
 
         await driver.findElement(By.id("num2")).click();
-        await driver.findElement(By.id("num2")).sendKeys("-2");
+        await driver.findElement(By.id("num2")).sendKeys("-1");
 
-        await driver.findElement(By.xpath("//button[text()='+']")).click();
+        await driver.findElement(By.xpath("//button[text()='-']")).click();
 
         const filename = 'test1';
         const encodedString = await driver.takeScreenshot();
